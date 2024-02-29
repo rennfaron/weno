300d3e2b0096dc5a26e5959be90b86f5d3fe1fa8
rennfaron <79808637+rennfaron@users.noreply.github.com>
2024-02-29 11:47:21 -0600

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,76 @@
+<?php include("header.php"); ?>
+        <section>
+            <header class="page">
+                <h1>
+                    Meet the team
+                </h1>
+                <h2>
+                    add copy about the team
+                </h2>
+            </header>
+        </section>
+    </div>
+	<section class="br-lightcyan">
+        <div class="xwide clearfix">
+        	<div class="our-team">
+                <ul>
+                    <li class="our-team-pic">
+                    	<img src="temp/about-us-filler-pic.jpg" />
+                    </li>
+                    <li>
+                    	<h2>Tina Goodman</h2>
+                    </li>
+                    <li>
+                    	<h3>CEO / PRESIDENT</h3>
+                    </li>
+                    <li>
+                    	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempor tortor ut felis vehicula pulvinar. Donec at nulla urna, id imperdiet odio. Fusce fringilla adipiscing auctor. Aenean porttitor aliquet velit, id bibendum sem aliquam congue. Vestibulum ut leo elit. Nam orci ligula, commodo vitae interdum et, auctor eu tum. Donec euismod elit quis mauris tempor vel posuere dolor iaculis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempor tortor ut felis vehicula pulvinar. Donec at nulla urna, id imperdiet odio. Fusce fringilla adipiscing auctor. Aenean porttitor aliquet velit, id bibendum sem aliquam congue. Vestibulum ut leo elit. Nam orci ligula, commodo vitae interdum et, auctor eu tum. Donec euismod elit quis mauris tempor vel posuere dolor iaculis.a</p>
+                    </li>
+                </ul>
+                <ul>
+                    <li class="our-team-pic">
+                    	<img src="temp/about-us-filler-pic.jpg" />
+                    </li>
+                    <li>
+                    	<h2>Ty Faulkner</h2>
+                    </li>
+                    <li>
+                    	<h3>VP WENO EXCHANGE BUSINESS DEVELOPMENT</h3>
+                    </li>
+                    <li>
+                    	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempor tortor ut felis vehicula pulvinar. Donec at nulla urna, id imperdiet odio. Fusce fringilla adipiscing auctor. Aenean porttitor aliquet velit, id bibendum sem aliquam congue. Vestibulum ut leo elit. Nam orci ligula, commodo vitae interdum et, auctor eu tum. Donec euismod elit quis mauris tempor vel posuere dolor iaculis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempor tortor ut felis vehicula pulvinar. Donec at nulla urna, id imperdiet odio. Fusce fringilla adipiscing auctor. Aenean porttitor aliquet velit, id bibendum sem aliquam congue. Vestibulum ut leo elit. Nam orci ligula, commodo vitae interdum et, auctor eu tum. Donec euismod elit quis mauris tempor vel posuere dolor iaculis.a</p>
+                    </li>
+                </ul>
+                <ul>
+                    <li class="our-team-pic">
+                    	<img src="temp/about-us-filler-pic.jpg" />
+                    </li>
+                    <li>
+                    	<h2>Kevin Pauley</h2>
+                    </li>
+                    <li>
+                    	<h3>EXECUTIVE ASSISTANT</h3>
+                    </li>
+                    <li>
+                    	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempor tortor ut felis vehicula pulvinar. Donec at nulla urna, id imperdiet odio. Fusce fringilla adipiscing auctor. Aenean porttitor aliquet velit, id bibendum sem aliquam congue. Vestibulum ut leo elit. Nam orci ligula, commodo vitae interdum et, auctor eu tum. Donec euismod elit quis mauris tempor vel posuere dolor iaculis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempor tortor ut felis vehicula pulvinar. Donec at nulla urna, id imperdiet odio. Fusce fringilla adipiscing auctor. Aenean porttitor aliquet velit, id bibendum sem aliquam congue. Vestibulum ut leo elit. Nam orci ligula, commodo vitae interdum et, auctor eu tum. Donec euismod elit quis mauris tempor vel posuere dolor iaculis.a</p>
+                    </li>
+                </ul>
+                <ul>
+                    <li class="our-team-pic">
+                    	<img src="temp/about-us-filler-pic.jpg" />
+                    </li>
+                    <li>
+                    	<h2>Jane Doe</h2>
+                    </li>
+                    <li>
+                    	<h3>POSITION HERE</h3>
+                    </li>
+                    <li>
+                    	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempor tortor ut felis vehicula pulvinar. Donec at nulla urna, id imperdiet odio. Fusce fringilla adipiscing auctor. Aenean porttitor aliquet velit, id bibendum sem aliquam congue. Vestibulum ut leo elit. Nam orci ligula, commodo vitae interdum et, auctor eu tum. Donec euismod elit quis mauris tempor vel posuere dolor iaculis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempor tortor ut felis vehicula pulvinar. Donec at nulla urna, id imperdiet odio. Fusce fringilla adipiscing auctor. Aenean porttitor aliquet velit, id bibendum sem aliquam congue. Vestibulum ut leo elit. Nam orci ligula, commodo vitae interdum et, auctor eu tum. Donec euismod elit quis mauris tempor vel posuere dolor iaculis.a</p>
+                    </li>
+                </ul>
+            </div>
+		</div>
+        <button type="button" class="go-top">Go Back to Top</button>
+    </section>
+<?php include("footer.php"); ?>
